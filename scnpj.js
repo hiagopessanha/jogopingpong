3c4c90c11fb44e9f90f8fed98785be08e1764756
hiagopessanha <161049581+hiagopessanha@users.noreply.github.com>
2024-09-06 11:58:57 -0300

    JOGO77

---
+++ scnpj.js
@@ -0,0 +1,17 @@
+//Elementos
+
+var vbtIniciar
+var vbola
+var vcpu
+var vjogador
+var vPaineltxtPontos
+
+//Controle de animação
+
+var game,frames
+
+//Posições
+
+var posBolaX,posBolaY
+var posJogadorX,posJogadorY
+var posCpuX,posCpuY
